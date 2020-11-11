39fc871d3ed437dda394002f9b1d853ac952c0da
dasjideepak <dasjideepak@gmail.com>
2020-11-11 15:01:51 +0530

    hello-world app

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to VueJS App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  padding: 12px 40px;
+  border: 1px solid black;
+}
+h1 {
+  font-size: 40px;
+  text-align: center;
+}
+</style>
