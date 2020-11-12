d0f73fcf24f370efb80e8ca034472b60976f817a
dasjideepak <dasjideepak@gmail.com>
2020-11-12 18:38:01 +0530

    todo-app initial commit

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Counter App" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -18,6 +19,8 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
